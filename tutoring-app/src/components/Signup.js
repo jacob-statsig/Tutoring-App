6190fc7e75d7dd0a957e82ec8ef1fca506d320b7
Jacob O'Quinn <jacob.oquinn374@gmail.com>
2021-11-27 12:57:22 -0500

    Adding subject selection

--- tutoring-app/src/components/Signup.js
+++ tutoring-app/src/components/Signup.js
@@ -9,16 +9,29 @@ export default function Signup() {
     const emailRef = useRef()
     const passwordRef = useRef()
     const passwordConfirmRef = useRef()
+    const tutorTopicRef = useRef()
+    const studentTopicRef = useRef()
     const { signUp, currentUser } = useAuth()
     const [error, setError] = useState('')
     const [loading, setLoading] = useState(false)
     const navigate = useNavigate()
+    
+    // TODO remove hard coded subjects
+    const subjects = ['Calc III', 'Comp. Sci. I', 'Spanish', 'Discrete']
+    const tutoringSubjects = []
+    const studentSubjects = []
+    
 
     async function handleSubmit(e) {
         e.preventDefault() // prevent refreshing
 
         if (passwordRef.current.value !== passwordConfirmRef.current.value) {
             return setError('Passwords do not match')
+        }
+
+        // check if at least 2 subjects are selected from each
+        if(tutoringSubjects.length < 2 || studentSubjects < 2) {
+            return setError('Please select two subjects from both fields')
         }
 
         try {
@@ -30,6 +43,32 @@ export default function Signup() {
             setError('Failed to create an account')
         }
         setLoading(false)
+    }
+
+    // This could cause errors where the screen displays checked but the subj hasn't been added to the appropriate array
+    function handleChange(e) {
+        // Check if updating student or tutoring
+        let arr = []
+        
+        // Remove / Add subject
+        let subj = e.target.id
+
+        if(e.target.id.includes("student")){
+            subj = subj.replace('student-','')
+            arr = studentSubjects
+        } else {
+            subj = subj.replace('tutor-','')
+            arr = tutoringSubjects
+        }
+
+        if(e.target.checked){
+            arr.push(subj)
+        } else {
+            const index = arr.indexOf(subj)
+            arr.splice(index, 1)
+        }
+        console.log(tutoringSubjects)
+        console.log(studentSubjects)
     }
 
     return (
@@ -51,6 +90,35 @@ export default function Signup() {
                         <Form.Group id="password-confirm">
                             <Form.Label>Password Confirmation</Form.Label>
                             <Form.Control type="password" ref={passwordConfirmRef} required />
+                        </Form.Group>
+
+                        <Form.Group id="tutor">
+                            <Form.Label>Topics You Can Teach</Form.Label>
+                            {subjects.map((topic) => (
+                                <div key={`tutor-${topic}`} className="mb-1">
+                                <Form.Check 
+                                    inline
+                                    type='checkbox'
+                                    onChange={e => handleChange(e)}
+                                    id={`tutor-${topic}`}
+                                    label={`${topic}`}
+                                />
+                                </div>
+                            ))}
+                        </Form.Group>
+                        <Form.Group id="student">
+                            <Form.Label>Topics You Need Help</Form.Label>
+                            {subjects.map((topic) => (
+                                <div key={`student-${topic}`} className="mb-1">
+                                <Form.Check 
+                                    inline
+                                    type='checkbox'
+                                    onChange={e => handleChange(e)}
+                                    id={`student-${topic}`}
+                                    label={`${topic}`}
+                                />
+                                </div>
+                            ))}
                         </Form.Group>
                         <Button disabled={loading} className="mt-4 w-100" type="submit">
                             Sign Up
