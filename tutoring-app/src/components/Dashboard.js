c5f76958db5c316b46d58c27a5c0b19a969915f1
Jacob O'Quinn <jacob.oquinn374@gmail.com>
2021-11-28 16:55:51 -0500

    init SearchResult

--- tutoring-app/src/components/Dashboard.js
+++ tutoring-app/src/components/Dashboard.js
@@ -1,12 +1,40 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import { Button, Card, Alert } from 'react-bootstrap'
 import { useAuth } from '../contexts/AuthContext'
 import { Link, useNavigate } from 'react-router-dom'
 import { auth, db } from '../firebase.js'
+import SearchResult from './SearchResult'
 
 export default function Dashboard() {
     const [error, setError] = useState('')
     const { logout, currentUser } = useAuth()
+
+    const [tutors, setTutors] = useState([])
+    const [loading, setLoading] = useState(false)
+
+    const ref = db.collection('users')
+    console.log(ref)
+
+    // get tutors when page first loads
+    useEffect(() => {
+        getTutors();
+    }, [])
+
+    function getTutors(){
+        setLoading(true);
+        // updates whenever db changes
+        ref.onSnapshot((querySnapshot) => {
+            const items = [];
+            // extract each document in folder
+            querySnapshot.forEach((doc) => {
+                items.push(doc.data());
+            })
+            setTutors(items);
+            setLoading(false);
+        })
+    }
+
+
     const navigate = useNavigate()
 
     async function handleLogout(){
@@ -19,10 +47,30 @@ export default function Dashboard() {
             setError('Failed to Log Out')
         }
     }
-
+    // TEMP: show if page is being updated
+    if(loading){
+        return <h1>Loading...</h1>
+    }
 
     return (
         <>
+            <div>
+                <h1>Tutors</h1>
+                {console.log(tutors)}
+                {tutors.map((tutor) => (
+
+                    
+
+                    <div key={tutor.uid}>
+                        <SearchResult 
+                        firstName={tutor.first_name}
+                        lastName={tutor.last_name}
+                        subjects={tutor.tutoring_subjects}
+                        />
+                    </div>
+                ))}
+            </div>
+
             <Card>
                 <Card.Body>
                     <h2 className="text-center mb-4">Profile</h2>
---
+++ tutoring-app/src/components/SearchResult.js
@@ -0,0 +1,126 @@
+import React, { useEffect } from 'react'
+import WebFont from 'webfontloader';
+
+
+export default function SearchResult(props) {
+
+    const { firstName, lastName, subjects, major, classOf } = props
+
+    useEffect(() => {
+        WebFont.load({
+            google: {
+                families: ['poppins']
+            }
+        });
+    }, []);
+      
+
+     const searchResult = {
+        alignItems: 'flexStart',
+        display: 'flex',
+        marginBottom: '8px',
+
+      }
+      
+      const overlapGroup = {
+        alignItems: 'flexEnd',
+        backgroundColor: 'skyBlue',
+        borderRadius: '26px',
+        display: 'flex',
+        height: '169px',
+        minWidth: '1125px',
+        padding: '26px 23px',
+      }
+      
+      const ellipse = {
+        alignSelf: 'center',
+        height: '106px',
+        marginBottom: '1.0px',
+        objectFit: 'cover',
+        width: '106px',
+      }
+      
+      const flexCol = {
+        alignItems: 'flexStart',
+        display: 'flex',
+        flexDirection: 'column',
+        marginLeft: '26px',
+        minHeight: '116px',
+        width: '461px',
+      }
+      
+      const offering = {
+        letterSpacing: '0',
+        minHeight: '47px',
+        width: '461px',
+        fontSize: '36px',
+        fontFamily: 'poppins',
+      }
+
+      const name = offering
+      
+      const majorStyle = {
+        letterSpacing: '0',
+        marginTop: '13px',
+        minHeight: '28px',
+        width: '225px',
+        fontSize: '18px',
+        fontFamily: 'poppins',
+      }
+      
+      const graduation = {
+        letterSpacing: '0',
+        minHeight: '28px',
+        width: '225px',
+        fontFamily: 'poppinsNormalCatskillWhite18px',
+      }
+      
+      const flexCol1 = {
+        alignItems: 'flexStart',
+        display: 'flex',
+        flexDirection: 'column',
+        minHeight: '116px',
+        width: '461px',
+      }
+      
+      const topics = {
+        letterSpacing: '0',
+        paddingTop: '5px',
+        PaddingBottom: '10px',
+        fontSize:'18px',
+        textColor: 'white',
+        fontFamily: 'poppins',
+      }
+
+    return (
+        <div style={searchResult}>
+            <div style={overlapGroup}>
+                <div style={flexCol}>
+                    <h1 style={name}>
+                        {firstName + " " + lastName}
+                    </h1>
+                    <div style={majorStyle}>
+                        {major ? major : ""}
+                    </div>
+                    <div style={graduation}>
+                        {classOf ? classOf : ""}
+                    </div>
+                </div>
+                <div style={flexCol1}>
+                    <div style={offering}>
+                        Offering
+                    </div>
+                    <div style={topics}>
+                        {subjects.map((subject) => (
+                            <div>
+                                {subject}
+                            </div>
+                        ))}
+                    </div>
+                    
+                </div>
+            </div>
+        </div>
+    )
+}
+
